ef40af66a7245b6a82d2e09e16d7163ce62f7512
Daniar <d-shakiev@mail.ru>
2021-01-14 19:20:50 +0500

    GO-22

---
+++ src/main/java/com/innopolis/eventgo/db/entity/Group.java
@@ -0,0 +1,31 @@
+package com.innopolis.eventgo.db.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Table(name = "group")
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Version
+    @Column(name = "version")
+    private Long version;
+}
---
+++ src/main/java/com/innopolis/eventgo/db/entity/Photo.java
@@ -0,0 +1,29 @@
+package com.innopolis.eventgo.db.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Table(name = "photo")
+public class Photo {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+
+    private String filename;
+}
